2ae841eea3712d6d15516f8ca1c14f5f46df63c8
Omar <101734132+OK3162022@users.noreply.github.com>
2022-11-12 15:52:48 +0200

    Update inference.py
    Wrapped chi2 test in Hotteling's T2 test

--- mva/inference.py
+++ mva/inference.py
@@ -12,13 +12,15 @@ def distance(data, MU):
     Sinv = np.linalg.inv(S) #inverse of S
     Xbar = np.mean(X, axis = 0).reshape(2,1)    #Mean vector
 
-    d2 = np.matmul(np.matmul((Xbar - MU).T, Sinv), (Xbar - MU))  #Mahalanobis distance squared
+    d2 = np.matmul(np.matmul((Xbar - MU).T, Sinv), (Xbar - MU))  #Mahalanobis distance
     
     return d2
 
-def hotteling1sample_test(data, MU):
+def hotteling1sample_test(data, MU, test = "Hotteling"):
     """
-    Tests hypothesis about the mean vector. use when sample size is smaller than 20p and the varaince-covariance marix is unknown  
+    Tests hypothesis about the mean vector. use when sample size is smaller than 20p
+    and the varaince-covariance matrix is unknown use the default (Hottelings)
+    if sample size is larger than 20p Hottelings T2 follows a chi-squared distribution.
     
     Parameters
     ---------- 
@@ -36,47 +38,34 @@ def hotteling1sample_test(data, MU):
     if np.any(data.dtypes == object):
         raise ValueError(f'All columns must be numeric')
 
-    n = data.shape[0]  #number of samples
-    p = data.shape[1]  #number of variables
-    df = n - p  #degrees of freedom
-    T2 = n * distance(data, MU) #Hotelling's T
-    f = ((n - p)/((n-1)*p)) * T2  #test statistic
-    pval = f.sf(f, p, df)   #pval
+    if test == "Hotteling":
+        n = data.shape[0]  #number of samples
+        p = data.shape[1]  #number of variables
+        df = n - p  #degrees of freedom
+        T2 = n * distance(data, MU) #Hotelling's T
+        f = ((n - p)/((n-1)*p)) * T2  #test statistic
+        pval = f.sf(f, p, df)   #pval
 
-    return f, pval
+        return f, pval
 
-def one_sample_chi2test(data, MU):
-    """
-    Tests hypothesis about the mean vector. use when sample size is larger than 20p 
-    
-    Parameters
-    ----------
-    data: data matrix
-    MU: mu vector
+    if test == "chi":
 
-    return
-    ------
-     test-statistic, pvalue
-
-
-    Note
-    -----
-    Use when sample size is large (n>20p)
-    """
-    if len(MU) != data.shape[1]:
-        raise ValueError(f"MU and number of variables should have the same length. Mu shape({MU.shape[0]}), Number of variables ({data.shape[1]})") 
-
-    if np.any(data.dtypes == object):
-        raise ValueError(f'All columns must be numeric')
-
-    n = data.shape[0]  #number of samples
-    p = data.shape[1]  #number of variables
-    chi2_calc = n * distance(data, MU)   #test statistic 
-    
-    pval = chi2.sf(chi2_calc, p)   #pval
-
-    return chi2_calc, pval
+        chi2_calc = n * distance(data, MU)   #test statistic 
         
+        pval = chi2.sf(chi2_calc, p)   #pval
+
+        return chi2_calc, pval
+        
+def s_pooled(data1, data2):
+    n1= len(data1)
+    n2= len(data2)
+    X1 = np.asmatrix(data1)                                   # convert first sample to matrix
+    S1 = np.cov(X1, rowvar=False)                             # variance covariance matrix of the first sample
+    X2 = np.asmatrix(data2)                                   # convert second sample to matrix
+    S2 = np.cov(X2, rowvar=False) 
+    S_pooled= ((n1-1)/(n1+n2-2))*S1 +((n2-1)/(n1+n2-2))*S2 
+    return  S_pooled, S1 , S2
+
 
 def hotteling2sample_paired_test(data1, data2): 
     """
@@ -111,9 +100,9 @@ def hotteling2sample_ind_ttest(data1, data2, equal_var = True):
     return None
     
 
-def spherecity_test(data): ##### The butcher was here 
+def spherecity_test(data):
     """
-    performs Maucly's sphercity test.
+    performs Mauchly's sphercity test.
     Checks if variables are independent and have the same variance
     
     Parameters
@@ -138,16 +127,6 @@ def spherecity_test(data): ##### The butcher was here
         raise ValueError(f'determinant of variance covariance atrix = 0 / variables maybe perfectly correlated')
     return calc_stat , pval
 
-def s_pooled(data1, data2):
-    n1= len(data1)
-    n2= len(data2)
-    X1 = np.asmatrix(data1)                                   # convert first sample to matrix
-    S1 = np.cov(X1, rowvar=False)                             # variance covariance matrix of the first sample
-    X2 = np.asmatrix(data2)                                   # convert second sample to matrix
-    S2 = np.cov(X2, rowvar=False) 
-    S_pooled= ((n1-1)/(n1+n2-2))*S1 +((n2-1)/(n1+n2-2))*S2 
-    return  S_pooled, S1 , S2
-
 
 def boxm_test(data1, data2):
     """
@@ -167,9 +146,5 @@ def boxm_test(data1, data2):
     num = (np.linalg.det(S1))**((n1-1)/2)*(np.linalg.det(S2))**((n2-1)/2) 
     dem= (np.linalg.det(S_pooled))**((n1+n2-2)/2)
     m= (num/dem)
-
-
-
-
-    #function
+    
     return None
