eb60b1ad94803cbd47b97d17044a17014b0cbd62
OK3162022 <o.abouyoussef73@gmail.com>
2022-11-19 18:08:05 +0200

    PCF factor and loadings

--- mva/factors.py
+++ mva/factors.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def _eigen_decompostion(X, cor = True):
     """
@@ -9,16 +10,16 @@ def _eigen_decompostion(X, cor = True):
     if cor == True:
         R = X.corr()
         lmbda, Q = np.linalg.eigh(R)
-        
-        return lmbda, Q 
 
-    else:     
+        return lmbda, Q
+
+    else:
         try:
             S = np.cov(X, rowvar=False)
             lmbda, Q = np.linalg.eigh(S)
             return lmbda, Q
 
-        except:   
+        except:
             raise ValueError(f'data matrix must be all numeric')
 
 #Principal components analysis
@@ -26,11 +27,11 @@ class PCA():
 
     def __init__(self, cor = True):
         self.cor = cor
-    
+
     def fit(self, X):
         self.eigenvals, self.eigenvectors = _eigen_decompostion(X, self.cor)
-        return self
-    
+        pass
+
     def summary(self):
         """eigenvalues table with % variance explained"""
         pass
@@ -38,10 +39,27 @@ class PCA():
     def scree_plot(self):
         pass
 
-    def fit_transform(self, n_components):
-        pass
-            
-            
+    def fit_transform(self, X, n_components):
+        """
+        Projects the data matrix onto the eigen vectors
+
+        Parameters
+        -----------
+        X: Data Matrix
+        n_components: number of princpal components
+
+        Return
+        ------
+        C: principal components dataframe 
+        """
+        if n_components > X.shape[1]:
+            raise ValueError(f'factors must be less than or equal number of variables')
+
+        C = np.matmul(X, self.eigenvectors[:,:n_components])
+        return C
+
+
+
 #################
 #Factor analysis
 #################
@@ -51,11 +69,16 @@ class PCF():
         self.cor = cor
 
     def fit(self, X):
+        self.X = X
         self.eigenvals, self.eigenvectors = _eigen_decompostion(X, self.cor)
         self.loadings =  self._estimate_loadings(self.eigenvals, self.eigenvectors)
         return self
-    
-    def loadings_table(self):
+
+    def _estimate_loadings(self, eigenvals, eigenvectors):
+        L = eigenvectors * np.sqrt(eigenvals)
+        return L
+
+    def summary(self):
         """
         loadings table
         """
@@ -63,15 +86,15 @@ class PCF():
 
     def _unique_factors(self):
         pass
-######
-    def fit_transform(self, eigenvals, eigenvectors, n_factors):
-        
-        #self.factors = _estimate_factors()
-        pass
+
+
+    def estimate_factors(self, eigenvals, eigenvectors, n_factors):
+        if n_factors > self.X.shape[1]:
+            raise ValueError(f'factors must be less or equal number of variables')
+
+        factors = PCA.fit_transform(n_factors) / np.sqrt(self.eigenvals)        
+        self.factors = factors        
+        return self.factors
 
     def rotate_factors(self):
         pass
-
-    def _estimate_loadings(eigenvals, eigenvectors):
-        L = np.matmul(np.sqrt(eigenvals), eigenvectors)
-        return L        
